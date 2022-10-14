624267b8cf0cf7d9bb58c20a63d961bec5e9e218
Nicholas Donahue <ndonahue95@gmail.com>
2022-10-14 15:15:06 -0400

    add interface to datastorebase

---
+++ src/DataAccess/IDataStoreBase.cs
@@ -0,0 +1,10 @@
+﻿using Google.Cloud.BigQuery.V2;
+
+namespace sharpbq.DataAccess;
+
+public interface IDataStoreBase
+{
+    List<T> Query<T>(string queryString, BigQueryClient client = null);
+
+    Task<List<T>> QueryAsync<T>(string queryString, List<BigQueryParameter> parameters = null, BigQueryClient client = null);
+}
