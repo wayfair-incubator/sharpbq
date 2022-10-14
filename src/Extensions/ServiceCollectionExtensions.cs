bdb33e273ed56e2e582597591b5ba9e971c94a9d
Anthony Forloney <aforloney@gmail.com>
2022-10-14 17:11:13 -0400

    adjusting src project to have a ServiceCollectionExtenstion to expose a helper method to register the necessary classes to container. Added dependencies to project as needed

---
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using sharpbq.Configuration;
+using sharpbq.DataAccess;
+
+namespace sharpbq.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddBigQueryClient(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddTransient<IBigQueryClientFactory, BigQueryClientFactory>();
+        services.AddTransient<IDataStoreBase, DataStoreBase>();
+        services.Configure<BigQueryProjectSettings>(options => configuration.GetSection("BigQuery").Bind(options));
+    }
+}
+
