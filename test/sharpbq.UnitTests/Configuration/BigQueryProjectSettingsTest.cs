a3fd8e184d6390e71d7d7202ab78259222fa51b1
Anthony Forloney <aforloney@gmail.com>
2022-10-14 14:53:04 -0400

    adding tests to confirm default settings behavior

---
+++ test/sharpbq.UnitTests/Configuration/BigQueryProjectSettingsTest.cs
@@ -0,0 +1,22 @@
+﻿using sharpbq.Configuration;
+using Shouldly;
+using Xunit;
+
+namespace sharpbq.UnitTests.Configuration;
+
+public class BigQueryProjectSettingsTest
+{
+    [Fact]
+    public void DefaultSettings_ShouldMatch()
+    {
+        // Arrange
+        BigQueryProjectSettings Subject = new();
+
+        // Act
+
+        // Assert
+        Subject.ProjectId.ShouldBeNull();
+        Subject.Credentials.ShouldBeNull();
+    }
+}
+
