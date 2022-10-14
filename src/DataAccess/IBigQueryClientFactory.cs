6c5c8d81cd63065acdc0a64631e2df57689124da
Nicholas Donahue <ndonahue95@gmail.com>
2022-10-14 16:09:25 -0400

    add factory and interfaces, to enable testing and DI config outside of datastore

---
+++ src/DataAccess/IBigQueryClientFactory.cs
@@ -0,0 +1,8 @@
+﻿using Google.Cloud.BigQuery.V2;
+
+namespace sharpbq.DataAccess;
+
+public interface IBigQueryClientFactory
+{
+    ISharpBQClient Create();
+}
---
+++ src/DataAccess/ISharpBQClient.cs
@@ -0,0 +1,17 @@
+﻿using Google.Apis.Bigquery.v2.Data;
+using Google.Cloud.BigQuery.V2;
+
+namespace sharpbq.DataAccess;
+
+public interface ISharpBQClient
+{
+    BigQueryResults ExecuteQuery(string sql, IEnumerable<BigQueryParameter> parameters,
+        QueryOptions queryOptions = null, GetQueryResultsOptions resultsOptions = null);
+    
+    Task<BigQueryResults> ExecuteQueryAsync(string sql, IEnumerable<BigQueryParameter> parameters,
+        QueryOptions queryOptions = null, GetQueryResultsOptions resultsOptions = null);
+    
+    BigQueryJob GetJob(JobReference jobReference, GetJobOptions options = null);
+    
+    Task<BigQueryJob> GetJobAsync(JobReference jobReference, GetJobOptions options = null);
+}
